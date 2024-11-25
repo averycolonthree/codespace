44973c594064f4dede18f33cdd0bc3dfa3470c15
averycolonthree <1103714@apps.nsd.org>
2024-11-25 21:07:26 +0000

    w

--- PairDemo.java
+++ PairDemo.java
@@ -1,1 +1,18 @@
+public class PairDemo {
+    public static void main(String[] args) {
+        
+        int[] target = {0, 0, 0};
+        int[] first = {0, 0, 0};
+        int[] second = {0, 0, 1};
+        int result = 0;
 
+        for(x = 0; x < 3; x++) {
+            if(first[x] == second[x]) {
+                x = x + 0;
+            } else {
+                int y = 
+                result
+            }
+        }
+    }
+}
