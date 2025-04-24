9d30c868d23f90d9724ba441800f7451f0034443
averycolonthree <1103714@apps.nsd.org>
2025-04-24 23:37:28 +0000

    uhhhh i dont know

--- PlanGen/PlanetGenerator.java
+++ PlanGen/PlanetGenerator.java
@@ -7,7 +7,29 @@ public class PlanetGenerator {
     
     Random rand = new Random();
     Scanner scanner = new Scanner(System.in);
-    String planetType = planetIsSmall();
+
+     // setting bounds for if planet is small or big
+     int tempBound;
+     int gravityBound;
+     int moonsBound;
+     int ringsBound;
+     String planetType;
+ 
+     if(rand.nextInt(2) == 1) {
+      // rocky planets like Earth
+       tempBound = 300;
+       gravityBound = 300;
+       moonsBound = 10;
+       ringsBound = 20;
+       planetType = "rocky";
+     } else {
+      // gas giants like Jupiter
+       tempBound = 5000;
+       gravityBound = 2500;
+       moonsBound = 300;
+       ringsBound = 2;
+       planetType = "gaseous";
+     }
     
     System.out.println("How many planets to make?");
     String numPlanets = scanner.nextLine();
@@ -19,15 +41,15 @@ public class PlanetGenerator {
       // there's gotta be a jollier way of doing this
         if(numPlanets.equals("temp")) {
           for(int i = 0; i < 100; i++) {
-            System.out.println(temp());
+            System.out.println(temp(rand, tempBound));
           }
         } else if(numPlanets.equals("gravity")) {
           for(int i = 0; i < 100; i++) {
-            System.out.println(gravity());
+            System.out.println(gravity(rand, gravityBound));
           }
         } else if(numPlanets.equals("moon")) {
           for(int i = 0; i < 100; i++) {
-            System.out.println(moon());
+            System.out.println(moons(rand, moonsBound));
           }
         }
       } else {
@@ -36,44 +58,37 @@ public class PlanetGenerator {
           System.out.println("========"); // divider
           System.out.println("Planet:");
           System.out.println("Type of planet: " + planetType);
-          System.out.println("Surface temp: " + temp() + "C");
-          System.out.println("Gravity: " + gravity + "g");
-          System.out.println("Amount of moons: " + moonCount);
-          if(hasRings) {
-            System.out.println("Planet has rings.");
-          }
-          if(isTidalLocked) {
-            System.out.println("Planet is tidally locked.");
-          }
+          System.out.println("Surface temp: " + temp(rand, tempBound) + "C");
+          System.out.println("Gravity: " + gravity(rand, gravityBound) + "g");
+          System.out.println("Amount of moons: " + moons(rand, moonsBound));
+          hasRings(rand, ringsBound);
+          isTidalLocked(rand);
         }
       }
     }
   // generate planet characteristics
-  static void temp(Random rand, )
+  static String temp(Random rand, int bound) {
+    return "Dummy";
+  }
 
+  static String gravity(Random rand, int bound) {
+    return "Dummy";
+  }
 
-  static void makePlanet(Random rand, boolean planetIsSmall) {
+  static String moons(Random rand, int bound) {
+    return "Dummy";
+  }
 
-    // setting bounds for if planet is small or big
-    int tempBound;
-    int gravityBound;
-    int moonsBound;
-    int ringsBound;
-    String planetType;
+  static String hasRings(Random rand, int bound) {
+    return "Dummy";
+  }
+  
+  static String isTidalLocked(Random rand) {
+    return "Dummy";
+  }
 
-    if(planetIsSmall) {     // rocky planets like Earth
-      tempBound = 300;
-      gravityBound = 300;
-      moonsBound = 10;
-      ringsBound = 20;
-      planetType = "rocky";
-    } else {                // gas giants like Jupiter
-      tempBound = 5000;
-      gravityBound = 2500;
-      moonsBound = 300;
-      ringsBound = 2;
-      planetType = "gaseous";
-    }
+  static void makePlanet(Random rand, boolean planetIsSmall, int tempBound, int gravityBound, int moonsBound, int ringsBound, String planetType) {
+
     
     // make temp by rolling 2 odds and averaging them
     int temp = Math.max(-274, ((rand.nextInt(tempBound) + 100) + (rand.nextInt(tempBound) + 100))/2 - 150);
