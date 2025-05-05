ba839d4fa53aa0e94d22e0a5e1fcfbf6e467102a
averycolonthree <1103714@apps.nsd.org>
2025-05-05 20:30:01 +0000

    star object

--- PlanGen2/PlanetGeneratorRev.java
+++ PlanGen2/PlanetGeneratorRev.java
@@ -3,7 +3,9 @@ import java.util.Random;
 public class PlanetGeneratorRev {
     public static void main(String[] args) {
 
-        Random rand = new Random();
+        Random rand = new Random(1);
+        Star star = new Star();
+
         for(int i = 0; i < 10; i++) {
 
             // range 0.1-10000
@@ -12,10 +14,11 @@ public class PlanetGeneratorRev {
 
             int radius = (int) countLowest(2, 191700-1900, rand) + 1900;
 
-            double mass = Math.round((countLowest(3, 4134.0-0.0041, rand) + 0.0041)*100);
+            double mass = Math.round((countLowest(5, 4134.0-0.0041, rand) + 0.0041)*100);
             mass = mass/100;
 
-            double gravity = gravity(radius, mass);
+            double gravity = Math.round(gravity(radius, mass)*10);
+            gravity = gravity/10;
 
             double surfaceTemp = surfaceTemp(orbitPeriod, rand)/100;
 
@@ -72,11 +75,13 @@ public class PlanetGeneratorRev {
         double boltzmannConstant = 5.670373e-8;
         // if we ever implement star stats, can be replaced w/ that
         double luminosity = rand.nextDouble()*10e27;
-        double albedo = 0.29; // could be randomized
-        double distance = 149e10;
+        double albedo = rand.nextInt(100)/100; // could be randomized
+
+        double minDist = 124764624.164;
+        double maxDist = 1.7951744e15;
+        double distance = ((rand.nextDouble()*maxDist)+minDist)-minDist;
 
         // https://astronomy.stackexchange.com/questions/10113/ formula
-        // TODO ask mctavish whether dist*dist or math.pow(dist, 2) is better
         double temp = Math.pow(
             ((luminosity*(1-albedo))/
             ((16*Math.PI)*(distance*distance)*boltzmannConstant)), 
@@ -84,14 +89,12 @@ public class PlanetGeneratorRev {
         return Math.round(temp*100);
     }
 
-    // bugged, only works if mass is less than 1 because of multiplication goofs
+    // slightly off i don't know why
     static double gravity(double radius, double mass) {
-        double earthMass = 5.9722e24;
-        //mass = mass * earthMass; // conversion to kg
-        radius = 58232;
-        mass = 5.6834e26;
+        double earthMass = 5.972168e24;
+        mass = mass * earthMass; // conversion to kg
         double newtonConstant = 6.67e-11;
-        return (newtonConstant*mass)/(radius*radius);
+        return ((newtonConstant*mass/(radius*radius))/1000000)*0.107;
     }
 
     static void printPlanet(double orbitPeriod, int radius, double mass, double gravity, double surfaceTemp) {
---
+++ PlanGen2/Star.java
@@ -0,0 +1,42 @@
+public class Star {
+
+    private double temperature;
+    private double luminosity;
+    private double radius;
+    private double mass;
+
+    // getters
+    public double getTemp() {
+        return temperature;
+    }
+    
+    public double getLuminosity() {
+        return luminosity;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    
+    public double getMass() {
+        return mass;
+    }
+
+    // setters
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+    
+    public void setLuminosity(double luminosity) {
+        this.luminosity = luminosity;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+}
