6e06536970ac0e8abfe7ad61d7b98b4179fa59bb
averycolonthree <1103714@apps.nsd.org>
2025-05-05 18:46:47 +0000

    more work

--- PlanGen2/PlanetGeneratorRev.java
+++ PlanGen2/PlanetGeneratorRev.java
@@ -4,12 +4,22 @@ public class PlanetGeneratorRev {
     public static void main(String[] args) {
 
         Random rand = new Random();
-        for(int i = 0; i < 1000; i++) {
-        double orbitPeriod = (countLowest(10, 100000, rand)*100)/1000;
-        double radius = countLowest(2, 191700-1900, rand) + 1900;
-        double surfaceTemp = surfaceTemp(orbitPeriod, rand)/100;
+        for(int i = 0; i < 10; i++) {
 
-        printPlanet(orbitPeriod, radius, surfaceTemp);
+            // range 0.1-10000
+            double orbitPeriod = Math.round((countLowest(12, 100000, rand)*10)/10); 
+            orbitPeriod = orbitPeriod/10;
+
+            int radius = (int) countLowest(2, 191700-1900, rand) + 1900;
+
+            double mass = Math.round((countLowest(3, 4134.0-0.0041, rand) + 0.0041)*100);
+            mass = mass/100;
+
+            double gravity = gravity(radius, mass);
+
+            double surfaceTemp = surfaceTemp(orbitPeriod, rand)/100;
+
+            printPlanet(orbitPeriod, radius, mass, gravity, surfaceTemp);
         }
     }
     
@@ -32,11 +42,11 @@ public class PlanetGeneratorRev {
         }
     
     // rolls numDice numbers within given bound and returns the highest one
-    static double countHighest(int numDice, int bound, Random rand) {
+    static double countHighest(int numDice, double bound, Random rand) {
         
-        int highestRoll = 0;
+        double highestRoll = 0;
         for(int i = 0; i < numDice; i++) {
-            int currentRoll = rand.nextInt(bound);
+            double currentRoll = rand.nextDouble()*bound;
             if(currentRoll > highestRoll) {
                 highestRoll = currentRoll;
             }
@@ -45,11 +55,11 @@ public class PlanetGeneratorRev {
     }    
 
     // rolls numDice numbers within given bound and returns the lowest one
-    static double countLowest(int numDice, int bound, Random rand) {
+    static double countLowest(int numDice, double bound, Random rand) {
         
-        int lowestRoll = bound;
+        double lowestRoll = bound;
         for(int i = 0; i < numDice; i++) {
-            int currentRoll = rand.nextInt(bound);
+            double currentRoll = rand.nextDouble()*bound;
             if(currentRoll < lowestRoll) {
                 lowestRoll = currentRoll;
             }
@@ -57,19 +67,39 @@ public class PlanetGeneratorRev {
         return lowestRoll;
     }    
 
+    // use some bs to calculate an average surface temp for a planet
     static double surfaceTemp(double orbitPeriod, Random rand) {
         double boltzmannConstant = 5.670373e-8;
-        double luminosity = rand.nextDouble()*10e27; // if we ever implement star stats, can be replaced w/ that
-        double albedo = 0.29;
-        double distance = 149e9;
+        // if we ever implement star stats, can be replaced w/ that
+        double luminosity = rand.nextDouble()*10e27;
+        double albedo = 0.29; // could be randomized
+        double distance = 149e10;
 
-        double temp = Math.pow(((luminosity*(1-albedo))/((16*Math.PI)*(distance*distance)*boltzmannConstant)), 0.25);
+        // https://astronomy.stackexchange.com/questions/10113/ formula
+        // TODO ask mctavish whether dist*dist or math.pow(dist, 2) is better
+        double temp = Math.pow(
+            ((luminosity*(1-albedo))/
+            ((16*Math.PI)*(distance*distance)*boltzmannConstant)), 
+            0.25);
         return Math.round(temp*100);
     }
 
-    static void printPlanet(double orbitPeriod, double radius, double surfaceTemp) {
+    // bugged, only works if mass is less than 1 because of multiplication goofs
+    static double gravity(double radius, double mass) {
+        double earthMass = 5.9722e24;
+        //mass = mass * earthMass; // conversion to kg
+        radius = 58232;
+        mass = 5.6834e26;
+        double newtonConstant = 6.67e-11;
+        return (newtonConstant*mass)/(radius*radius);
+    }
+
+    static void printPlanet(double orbitPeriod, int radius, double mass, double gravity, double surfaceTemp) {
+        System.out.println("===================");
         System.out.println("Orbital period: " + orbitPeriod + " days");
         System.out.println("Radius: " + radius + " km");
+        System.out.println("Mass: " + mass + " M🜨");
+        System.out.println("Gravity: " + gravity + "g");
         System.out.println("Average surface temperature: " + surfaceTemp + "K");
     }
 }
